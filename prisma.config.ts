2f7d338a6f237ad60800651b560e88ea9ec758d9
FlyingFork <FlyingFork@protonmail.com>
2026-07-19 01:34:04 +0300

    fix: Prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,13 @@
 import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
-import { env } from './src/config/env';
+import { z } from 'zod';
+
+const prismaEnvSchema = z.object({
+  DATABASE_URL: z.string().url(),
+});
+
+const env = prismaEnvSchema.parse(process.env);
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
